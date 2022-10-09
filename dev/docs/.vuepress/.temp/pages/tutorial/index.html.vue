6f10740018e56c2726d816e937b8f284dc4efd9b
donnyjin <jin52627042@gmail.com>
2022-10-09 10:40:09 +0700

    2022-10-9 10:40:06

--- dev/docs/.vuepress/.temp/pages/tutorial/index.html.vue
+++ dev/docs/.vuepress/.temp/pages/tutorial/index.html.vue
@@ -1,5 +1,6 @@
 <template><div><h1 id="星际公民教程" tabindex="-1"><a class="header-anchor" href="#星际公民教程" aria-hidden="true">#</a> 星际公民教程</h1>
-<h2 id="_1-解决账号解决注册星际公民账号无限加载问题" tabindex="-1"><a class="header-anchor" href="#_1-解决账号解决注册星际公民账号无限加载问题" aria-hidden="true">#</a> 1.解决账号解决注册星际公民账号无限加载问题</h2>
+<h2 id="_1-账号注册与购买" tabindex="-1"><a class="header-anchor" href="#_1-账号注册与购买" aria-hidden="true">#</a> 1.账号注册与购买</h2>
+<h3 id="_1-解决注册星际公民账号无限加载问题" tabindex="-1"><a class="header-anchor" href="#_1-解决注册星际公民账号无限加载问题" aria-hidden="true">#</a> ①.解决注册星际公民账号无限加载问题</h3>
 <p>  之所以会遇到这种情况,一般说明你需要魔法上网才能解决了.有一款可以免费试用的google浏览器(chrome)插件,应该可以帮到您.插件名叫setupVPN.
 <a href="https://starcitizenzh.com/files/SetupVPN_v3.11.9.zip" target="_blank" rel="noopener noreferrer">setupVPN插件压缩包文件下载<ExternalLinkIcon/></a></p>
 <p>插件安装教学:<br>
@@ -7,10 +8,13 @@
 第二步:打开chrome浏览器在地址栏输入 chrome://extensions/ 后回车,进入扩展程序页面.<br>
 第三部:加载扩展程序的文件夹.<br>
 <img src="/images/tutorial/setupVPN_install/01.jpg" alt="01"><br>
-<img src="/images/tutorial/setupVPN_install/02.jpg" alt="01"><br>
+<img src="/images/tutorial/setupVPN_install/02.jpg" alt="02"><br>
 第四部:安装完毕后,会出现相关的插件信息.然后在浏览器的右上角找到 扩展程序 按钮,就可以开始使用了.<br>
-<img src="/images/tutorial/setupVPN_install/03.jpg" alt="01"><br>
-<img src="/images/tutorial/setupVPN_install/04.jpg" alt="01"></p>
-<!-- ![VuePress Logo](/images/starcitizenzh.png) --></div></template>
+<img src="/images/tutorial/setupVPN_install/03.jpg" alt="03"><br>
+<img src="/images/tutorial/setupVPN_install/04.jpg" alt="04"></p>
+<h3 id="_2-游戏购买方式" tabindex="-1"><a class="header-anchor" href="#_2-游戏购买方式" aria-hidden="true">#</a> ②.游戏购买方式</h3>
+<p>  目前官方只支持信用卡和Paypal,其中Paypal还支持国内的银联借记卡.
+<img src="/images/tutorial/pay_game/pay_method.jpg" alt="pay_method"></p>
+</div></template>
 
 
--- dev/docs/tutorial/README.md
+++ dev/docs/tutorial/README.md
@@ -1,6 +1,7 @@
 # 星际公民教程
 
-## 1.解决账号解决注册星际公民账号无限加载问题
+## 1.账号注册与购买
+### ①.解决注册星际公民账号无限加载问题
 &emsp;&emsp;之所以会遇到这种情况,一般说明你需要魔法上网才能解决了.有一款可以免费试用的google浏览器(chrome)插件,应该可以帮到您.插件名叫setupVPN.
  [setupVPN插件压缩包文件下载](https://starcitizenzh.com/files/SetupVPN_v3.11.9.zip)
 
@@ -9,11 +10,11 @@
  第二步:打开chrome浏览器在地址栏输入&nbsp;chrome://extensions/&nbsp;后回车,进入扩展程序页面.  
  第三部:加载扩展程序的文件夹.  
 ![01](/images/tutorial/setupVPN_install/01.jpg)  
-![01](/images/tutorial/setupVPN_install/02.jpg)  
+![02](/images/tutorial/setupVPN_install/02.jpg)  
 第四部:安装完毕后,会出现相关的插件信息.然后在浏览器的右上角找到 扩展程序 按钮,就可以开始使用了.  
-![01](/images/tutorial/setupVPN_install/03.jpg)  
-![01](/images/tutorial/setupVPN_install/04.jpg)
+![03](/images/tutorial/setupVPN_install/03.jpg)  
+![04](/images/tutorial/setupVPN_install/04.jpg)
 
-
-
-<!-- ![VuePress Logo](/images/starcitizenzh.png) -->
+### ②.游戏购买方式  
+&emsp;&emsp;目前官方只支持信用卡和Paypal,其中Paypal还支持国内的银联借记卡.
+![pay_method](/images/tutorial/pay_game/pay_method.jpg)
